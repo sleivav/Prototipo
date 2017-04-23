4ee3aec34d022fe31989d5c2ce17bfe72bef9567
valentina <valentinaliberona@hotmail.com>
2017-04-23 10:11:20 -0300

    added login/signing and vendedor templates

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,10 @@ from app import views
 urlpatterns = [
     #/app/
     url(r'^$', views.index, name='index'),
+    #/app/login
+    url(r'^login$', views.login, name='login'),
+    #/app/signup
+    url(r'^signup$', views.signup, name='signup'),
+    #/app/vendedor
+    url(r'^vendedor$', views.vendedorprofilepage, name='vendedorprofilepage'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html', {})
+
+def login(request):
+    return render(request, 'app/login.html', {})
+
+def signup(request):
+    return render(request, 'app/signup.html', {})
+
+def vendedorprofilepage(request):
+    return render(request, 'app/vendedor-profile-page.html', {})
